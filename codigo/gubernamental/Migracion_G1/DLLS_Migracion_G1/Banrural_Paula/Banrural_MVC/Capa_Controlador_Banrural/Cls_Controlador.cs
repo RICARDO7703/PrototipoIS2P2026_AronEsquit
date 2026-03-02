78a90dded9fc0970cc37ceb66b0ecbc807707403
IngPau <paulaleonardo289@gmail.com>
2026-03-02 15:34:51 -0600

    Cls_Controlador se agregaron los controladores

--- codigo/gubernamental/Migracion_G1/DLLS_Migracion_G1/Banrural_Paula/Banrural_MVC/Capa_Controlador_Banrural/Cls_Controlador.cs
+++ codigo/gubernamental/Migracion_G1/DLLS_Migracion_G1/Banrural_Paula/Banrural_MVC/Capa_Controlador_Banrural/Cls_Controlador.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using Capa_Modelo_Banrural;
 
 namespace Capa_Controlador_Banrural
 {
-    class Cls_Controlador
+    public class Cls_Controlador
     {
+        private readonly Cls_Sentencias sen = new Cls_Sentencias();
+
+        public DataTable BuscarCiudadanoPorDpi(long dpi)
+        {
+            return sen.BuscarCiudadanoPorDpi(dpi);
+        }
+
+        public DataTable ObtenerTiposPasaporte()
+        {
+            return sen.ObtenerTiposPasaporte();
+        }
+
+        public DataTable ObtenerDuracionesPorTipo(int idTipo)
+        {
+            return sen.ObtenerDuracionesPorTipo(idTipo);
+        }
+
+        public decimal ObtenerPrecioTipo(int idTipo)
+        {
+            return sen.ObtenerPrecioPorTipo(idTipo);
+        }
+
+        public int GuardarBoleta(int numeroBoleta, int idCiudadano, int idTipo)
+        {
+            return sen.InsertarBoleta(numeroBoleta, idCiudadano, idTipo);
+        }
+
+        public bool ExisteNumeroBoleta(int numeroBoleta)
+        {
+            return sen.ExisteNumeroBoleta(numeroBoleta);
+        }
     }
 }
